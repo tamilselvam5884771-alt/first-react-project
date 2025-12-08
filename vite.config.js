0641ccbda598a934fe07d213c63f2175d878a95d
HARIHARASUDHAN <tamilselvam5884771@gmail.com>
2025-12-08 10:14:11 +0530

    Update project and deploy

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/hariharasudhan/',
+    base: '/first-react-project/', // 🔥 must match GitHub repo name
     plugins: [react()],
     build: {
         rollupOptions: {
             output: {
                 manualChunks(id) {
                     if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
                         if (id.includes('swiper'))
                             return 'swiper';
                         return;
